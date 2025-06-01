4b2793037533e6696ad4a9bdfc8844c77aff6035
SAJI <ut01261tic2024@gmail.com>
2025-06-01 22:44:24 +0530

    practice a class

---
+++ Practice console app/service.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_console_app
+{
+    
+    public class book
+    {
+        public int bookid { get; }
+
+        public book(int Bookid)
+        {
+            bookid = Bookid;
+        }
+
+        private string title;
+        public string Title
+        {
+            //get { return title; } is used so that you can safely and flexibly let outside code read the value of the private title field.
+            get { return title; }   
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Title cannot be empty or whitespace.");
+                }
+                title = value;
+            }
+        }
+
+    }
+
+    
+}
+    
